9ae87e8a53916ee1763a265b77168836a59e8901
Edward <pakachun48@gmail.com>
2025-09-05 14:29:02 +0300

    feat: add Gradle and Hibernate setup

--- src/Main.java
+++ src/Main.java
@@ -14,18 +14,5 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
-
-        DatabaseConnection databaseConnection = new DatabaseConnection();
-
-        UserRepository userRepository = new UserRepository(databaseConnection);
-
-        try {
-            User newUser = new User("Eduard", "Kirov", "edwardmar", "edik200331", LocalDate.of(2002,7,31));
-
-            userRepository.create(newUser);
-
-        } catch (DatabaseException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
