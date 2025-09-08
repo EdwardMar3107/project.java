4a538225f072fa02fe36f630bbe43be2e3fcb5bd
Edward <pakachun48@gmail.com>
2025-09-08 18:41:41 +0300

    test: verify data persistence after fixes

--- src/main/java/by/ezer/utils/HibernateUtil.java
+++ src/main/java/by/ezer/utils/HibernateUtil.java
@@ -13,7 +13,6 @@ public class HibernateUtil {
         try {
             return new Configuration().configure().buildSessionFactory();
         } catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
