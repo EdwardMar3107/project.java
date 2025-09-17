6b0c4197b3ab4b89744f2675ec4a85024bd9415f
Edward <pakachun48@gmail.com>
2025-09-17 18:55:53 +0300

    Transactions

--- src/main/java/by/ezer/service/UserService.java
+++ src/main/java/by/ezer/service/UserService.java
@@ -3,6 +3,7 @@ package by.ezer.service;
 import by.ezer.dto.userDTO.UserCreateDTO;
 import by.ezer.dto.userDTO.UserDTO;
 import by.ezer.exceptions.DatabaseException;
+import by.ezer.exceptions.RepositoryException;
 import by.ezer.models.User;
 import by.ezer.repositories.api.UserRepository;
 import org.hibernate.Session;
@@ -16,9 +17,9 @@ public class UserService {
         this.userRepository = new by.ezer.repositories.impl.UserRepositoryImpl(session);
     }
 
-    public UserDTO createUser(UserCreateDTO userCreateDTO) throws DatabaseException {
+    public UserDTO createUser(UserCreateDTO userCreateDTO) throws RepositoryException {
         if (userCreateDTO == null) {
-            throw new DatabaseException("UserCreateDTO cannot be null");
+            throw new RepositoryException("UserCreateDTO cannot be null");
         }
         User user = new User(
                 userCreateDTO.getName(),
@@ -31,28 +32,28 @@ public class UserService {
         return new UserDTO(user.getId(), user.getName(), user.getSurname(), user.getBirthDate());
     }
 
-    public UserDTO getUserById(Long id) throws DatabaseException {
+    public UserDTO getUserById(Long id) throws RepositoryException {
         User user = userRepository.findById(id);
         if (user == null) {
-            throw new DatabaseException("User with id " + id + " not found");
+            throw new RepositoryException("User with id " + id + " not found");
         }
         return new UserDTO(user.getId(), user.getName(), user.getSurname(), user.getBirthDate());
     }
 
-    public List<UserDTO> getAllUsers() throws DatabaseException {
+    public List<UserDTO> getAllUsers() throws RepositoryException {
         List<User> users = userRepository.findAll();
         return users.stream()
                 .map(user -> new UserDTO(user.getId(), user.getName(), user.getSurname(), user.getBirthDate()))
                 .collect(Collectors.toList());
     }
 
-    public void updateUser(UserDTO userDTO) throws DatabaseException {
+    public void updateUser(UserDTO userDTO) throws RepositoryException {
         if (userDTO == null || userDTO.getId() == null) {
-            throw new DatabaseException("UserDTO or ID cannot be null");
+            throw new RepositoryException("UserDTO or ID cannot be null");
         }
         User existingUser = userRepository.findById(userDTO.getId());
         if (existingUser == null) {
-            throw new DatabaseException("User with ID " + userDTO.getId() + " not found");
+            throw new RepositoryException("User with ID " + userDTO.getId() + " not found");
         }
         existingUser.setName(userDTO.getName());
         existingUser.setSurname(userDTO.getSurname());
@@ -60,7 +61,7 @@ public class UserService {
         userRepository.update(existingUser);
     }
 
-    public void deleteUser(Long id) throws DatabaseException {
+    public void deleteUser(Long id) throws RepositoryException {
         userRepository.delete(id);
     }
 }
