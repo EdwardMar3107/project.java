4d4d5314acc081aa4ed367ae7d78a49c0f37e9e0
Edward <pakachun48@gmail.com>
2025-11-22 13:35:16 +0300

    Fixes

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,8 @@
-import exceptions.DatabaseException;
-import models.Order;
-import models.Product;
-import models.User;
-import repositories.OrderRepository;
-import repositories.ProductRepository;
-import repositories.UserRepository;
-import utils.DatabaseConnection;
 
-import javax.xml.crypto.Data;
-import java.math.BigDecimal;
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, DatabaseException {
+    public static void main(String[] args) {
 
-        DatabaseConnection databaseConnection = new DatabaseConnection();
 
-        OrderRepository orderRepository = new OrderRepository(databaseConnection);
-
-        ProductRepository productRepository = new ProductRepository(databaseConnection);
-
-        Order byId = orderRepository.findById(1L);
-
-        System.out.println(byId);
     }
 }
