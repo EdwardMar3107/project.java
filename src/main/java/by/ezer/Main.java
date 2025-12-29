b2030a48965de68c2e531691f4062cbb3980a886
Edward <pakachun48@gmail.com>
2025-12-30 01:42:17 +0300

    Hibernate Repo

--- src/main/java/by/ezer/Main.java
+++ src/main/java/by/ezer/Main.java
@@ -3,6 +3,9 @@ package by.ezer;
 import by.ezer.entity.Order;
 import by.ezer.entity.Product;
 import by.ezer.entity.User;
+import by.ezer.repository.OrderRepository;
+import by.ezer.repository.ProductRepository;
+import by.ezer.repository.UserRepository;
 import by.ezer.util.HibernateUtil;
 import jakarta.persistence.EntityManager;
 
@@ -11,8 +14,26 @@ import java.time.LocalDateTime;
 
 public class Main {
     static void main() {
-        try (EntityManager em = HibernateUtil.getEntityManager()) {
-            em.getTransaction().begin();
-        }
+        UserRepository userRepo = new UserRepository();
+        ProductRepository productRepo = new ProductRepository();
+        OrderRepository orderRepo = new OrderRepository();
+
+        User us1 = new User("Eduard", 23, "pakachun48@gmail.com");
+        Product pr1 = new Product("PC", new BigDecimal("5000.00"), "Cool");
+        Product pr2 = new Product("Mouse", new BigDecimal("50.00"), "Small");
+        Order order = new Order(LocalDateTime.now(), new BigDecimal("5050.00"));
+
+//        1
+        us1.addOrder(order);
+        order.setUser(us1);
+
+//        2
+        order.addProduct(pr1);
+        order.addProduct(pr2);
+
+//        3
+        userRepo.save(us1);
+
+        System.out.println("All saved, order id: " + order.getId());
     }
 }
--- src/main/java/by/ezer/entity/User.java
+++ src/main/java/by/ezer/entity/User.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@ToString(exclude = "orders")
 public class User {
 
     @Id
